751f02285a35641baf1675a82fad0920315ef128
femdomideagenerator <34683974+femdomideagenerator@users.noreply.github.com>
2017-12-19 10:38:12 -0500

    Add files via upload

---
+++ FemDomIdeaGenerator 1.0.1.py
@@ -0,0 +1,192 @@
+print ("""Welcome To The Female Domination Idea Generator.\n 
+Not Sure How To Dominate Your Sub?\n
+Enter A Number From The Following Kinks, And It'll Generate An Idea To Help You Play With Your Sub.\n
+Numbers:  1 - 11  = Orgasm Denial/Tease and Denial
+Numbers: 12 - 28 = Humiliation/Domination/Power Exchange
+Numbers: 29 - 35 = Pain/Impact Play
+Numbers: 36 - 40 = For Domme's Pleasure
+Numbers: 41 - 47 = Bondage
+Numbers: 48 - 54 = Foot Fetish
+Numbers: 55 - 63 = Services
+""")
+
+def enter_number():
+	var = int(input("Please Enter A Number: "))
+	if var == 1:
+		print("\nCategory [Denial]: Tell Your Sub He Isn't Allowed To Cum For [3 Days].\n")
+	if var == 2:
+		print("\nCategory [Denial]: Tell Your Sub He Isn't Allowed To Cum For [6 Hours].\n")
+	if var == 3:
+		print("\nCategory [Denial]: Tell Your Sub He Isn't Allowed To Cum For [7 Days].\n")
+	if var == 4:
+		print("\nCategory [Denial]: Tell Your Sub He Isn't Allowed To Cum For [14 Days].\n")
+	if var == 5:
+		print("""\nCategory [Denial]: Cuddle With Your Sub, With Him As The Little Spoon. Reach Around And Hold His Dick In Your Hand.
+Once He's Hard He May Try To Thrust His Hips To Get Some Friction/Pleasure. If He Does, Tell Him To Stop, And He's Not
+Allowed To Move. [Duration: However Long You Want]\n """)
+	if var == 6:
+		print("""\nCategory [Denial]: Hang Out In Your Sexiest Outfit Or Naked, Whatever Turns Your Sub On More. Tease Your Sub,
+And When He Tries To Touch You, Tell Him "No Touching" And Only Allow Him To Look and Stare.
+[Duration: As Long As You Want]\n""")
+	if var == 7:
+		print("""\nCategory [Denial]: Make Your Sub Sit With His Face Right In-front Of Your Pussy, Close Enough So You Can
+Feel His Breath. Make Him Smell It, And Stare At It. Remind Him He's A Slave To It, And It Owns Him.
+If You Want To Be Extra Cruel, Ask Him If He Would Like To Lick It, Once He Says Yes. Tell Him "Too Bad."
+Keep Teasing Him With It, Grind It In-front Of His Face, Get Him Begging To Eat It.
+Once You Think He's Had Enough Tell Him To Give It A Kiss And Then He Can Go Back To Doing Whatever He Was Doing Before.""")
+	if var == 8:
+		print("""\nCategory [Denial]: Grind Your Pussy On Your Sub's Face. (You Can Have Him Stick His Tongue Out If It's Better For You.)
+But The Point Is To Use His Face To Get Your Self Off, And Cum All Over His Face. Make Him Feel Used.
+Once You've Came On His Face, Tell Him To Go And Get Cleaned Up, Or Tell Him He Has To Wait To Wash His Face
+For However Long You Want, So All He Can Smell Is You. Mark Your Property With Your Scent.\n""")
+	if var == 9:
+		print("""\nCategory [Denial]: Masturbate In-front Of Your Sub. Get Him Hard And Horny. Tell Him He's Not Allowed To Touch His Cock
+Until You've Had An Orgasm. Tell Him How Much You Love Being Able To Pleasure Yourself, While He Has To Wait To Get
+Your Permission To Touch Himself. If He Forgets, Or Just Can't Help Himself, And Starts Jerking Off,
+Punish Him How You Want. Give Yourself As Many Orgasms As You Want, Before He Can Have His.
+If You Want To Be Extra Cruel, After You Came, Tell Him You Changed Your Mind, And He's Not Allowed To Cum.\n""")
+	if var == 10:
+		print("\nCategory [Denial]: Give Your Sub A Handjob For [20 Minutes] And Tell Him He CANNOT Cum. If He Does, Punish Him.\n")
+	if var == 11:
+		print("\nCategory [Denial]: Tease Your Sub For [However Long You Want], But Don't Allow Him To Cum, If He Does, Punish Him.\n")
+	if var == 12:
+		print("\nCategory [Humiliation]: Spit On Your Sub's Face, Don't Let Him Wipe It Off For [However Long You Want]\n")
+	if var == 13:
+		print("""\nCategory [Humiliation]: Call Your Sub Over By Calling Him Something Demeaning, And Tell Him He Better Get His Ass
+In The Kitchen, And Get Your Dinner Ready. Or If You're In The Mood For Fast Food, Tell Him To Go Pickup Food For You.
+If You're Feeling Really Bitchy, Tell Him He's Not Allowed To Get Anything For Himself.\n""")
+	if var == 14:
+		print("""\nCategory [Humiliation]: If Your Sub Has A Tumblr, Make Him Login, And Give You The Device So You Can Go Through All
+His Recent Likes, And See What Kinds Of Things He's Been Looking At. If He Bookmarks His Porn In His Browser,
+You Can Do The Same Thing. Additionally, You Can Question Him, And Make Him Tell You Why He Liked Each Post,
+Or Bookmarked Each Video.
+(Bonus Points If You Get Him Blushing And Stuttering.)\n""")
+	if var == 15:
+		print("\nCategory [Humiliation]: Make Your Sub Eat His Next Meal Off Your Feet.\n")
+	if var == 16:
+		print("""\nCategory [Humiliation]: Tell Your Sub He's Not Allowed To Eat His Food, Until You've Finished Eating.
+Additionally You Can Comment About How Good It Is, And Stuff Like That\n""")
+	if var == 17:
+		print("\nCategory [Humiliation]: For [1 Hour] Your Sub Is Not Allowed To Speak Unless Spoken To Directly By You.\n")
+	if var == 18:
+		print("""\nCategory [Humiliation]: For [1 Hour] Your Sub Must Ask "Permission To Speak Ma'am?" Before Saying Or Asking
+Anything Else. You Can Permit Or Deny Him To Talk Depending On If You're In The Mood To Hear What He Has To Say.\n""")
+	if var == 19:
+		print("""\nCategory [Humiliation]: Spit In Your Sub's Mouth, And Make Him Swallow It. If He Refuses, Hold His Nose,
+And Force Him To Open His Mouth. (If He Does Refuse, A Nice Slap To The Face Might Remind Him Whose In Charge.)\n""")
+	if var == 20:
+		print("""\nCategory [Humiliation]: Tell Your Sub To Get On His Knees, Lower Your Pants, And Kiss Your Ass
+[Duration: However Long You Want]\n""")
+	if var == 21:
+		print("""\nCategory [Humiliation]: Send Your Sub To Bed Early, Like A Child. Tell Him If He Isn't Asleep By A Certain Time,
+He Will Get A Spanking. If He Complains, Or Refuses, Punish Accordingly.\n""")
+	if var == 22:
+		print("\nCategory [Humiliation]: Make Your Sub Tell You One Of Darkest Fantasies. Enjoy Watching Him Blush and Stutter.\n")
+	if var == 23:
+		print("""\nCategory [Humiliation]: Ask Your Sub Any Question You Like, Tell Him He Has To Answer Truthfully.
+If You Find Out He Is Lying, Punish Accordingly.\n""")
+	if var == 24:
+		print("""\nCategory [Humiliation]: Tell Your Sub He Must Respond To You With Yes Ma'am, or No Ma'am, To Show You Some Respect
+[For 1 Hour] If He Forgets To Ask, Punish Accordingly\n""")
+	if var == 25:
+		print("""\nCategory [Humiliation]: Tell Your Sub He Must Respond To You With Yes Ma'am, or No Ma'am, To Show You Some Respect
+[For As Long As You Want] If He Forgets, Punish Accordingly.\n""")
+	if var == 26:
+		print("""\nCategory [Humiliation]: Anytime You Or Your Sub Leaves or Enters A Room, Your Sub Must Kiss Both Of Your Feet Once
+[For 1 Day Or As Long As You Want]\n""")
+	if var == 27:
+		print("\nCategory [Humiliation]: Force Your Sub To Be Naked While You're Dressed For [As Long As You Want]\n")
+	if var == 28:
+		print("""\nCategory [Humiliation]: If You're Out In Public Try To Get Your Sub Hard, And Tease/Deny Him.
+Watch Him Squirm While Trying To Hide His Hard-On\n""")
+	if var == 29:
+		print("\nCategory [Pain]: Spank Your Sub Over Your Knee For [5 Minutes]\n")
+	if var == 30:
+		print("\nCategory [Pain]: Spank Your Sub Over Your Knee For [10 Minutes]\n")
+	if var == 31:
+		print("\nCategory [Pain]: Spank Your Sub Over Your Knee [Until You're Satisfied]\n")
+	if var == 32:
+		print("\nCategory [Pain]: Face Slapping [5 Times]\n")
+	if var == 33:
+		print("\nCategory [Pain]: Face Slapping [10 Times]\n")
+	if var == 34:
+		print("\nCategory [Pain]: Face Slapping [However Many Times You Want]\n")
+	if var == 35:
+		print("\nCategory [Pain]: Spank Your Sub Bent Over The Bed, Or Desk [Until You're Satisfied]\n")
+	if var == 36:
+		print("""\nCategory: [Domme's Pleasure] (Make It All About You): Tell Your Sub To Give You Any Kind Of Massage You Want
+Until You're Satisfied. If Your Sub Starts To Get A Little Handsy On The Wrong Body Parts,
+Don't Be Afraid To Correct His Behavior.\n""")
+	if var == 37:
+		print("""\nCategory: [Domme's Pleasure] (Make It All About You): Tell Your Sub You Want A Certain Amount Of Orgasms
+In A Time-Frame\n""")
+	if var == 38:
+		print("""\nCategory: [Domme's Pleasure] (Make It All About You) Tell Your Sub To Get Over Here And Eat Your Pussy,
+If He Doesn't Want To Get Punished.\n""")
+	if var == 39:
+		print("""\nCategory: [Domme's Pleasure] (Make It All About You): Tell Your Sub To Get Over Here And Eat Your Ass,
+And He Better Do A Good Job.\n""")
+	if var == 40:
+		print("""\nCategory: [Domme's Pleasure] (Make It All About You): He Is Now Your Slave For [1 Hour] Make Him Do Whatever You Want
+(Cleaning, Chores, Etc.)\n""")
+	if var == 41:
+		print("\nCategory [Bondage]: Tie Your Sub To A [Chair] And Do Anything You Want For [20 Minutes]\n")
+	if var == 42:
+		print("""\nCategory [Bondage]: Put A Ball Gag In Your Sub's Mouth For [However Long You Don't Want To Hear Him]
+And Do Whatever You Wish To Him.\n""")
+	if var == 43:
+		print("""\nCategory [Bondage]: Gag Your Sub's Mouth With Your [Dirty Socks] For [However Long You Want To Humiliate Him]
+And Do Whatever You'd Like To Him. Make Sure To Tease Him, And Ask Him How Your Socks Taste, And Ask If He Likes Them.\n""")
+	if var == 44:
+		print("""\nCategory [Bondage]: Gag Your Sub's Mouth With Your [Worn Panties] For [However Long You Want To Humiliate Him]
+And Do Whatever You'd Like To Him. Make Sure To Tease Him, And Ask Him How Your Panties Taste, And Ask If He Likes It.\n""")
+	if var == 45:
+		print("""\nCategory [Bondage]: Straddle Your Sub's Chest, And Put Your Hand Over Your Sub's Mouth To Dominate Them For
+[However Long You Want] You Can Tease Him By Asking Him To Try To Speak, Or Tell Him You Decide When He's
+Allowed To Talk. You Can Also Tie Up His Hands So It's Harder For Him To Struggle.\n""")
+	if var == 46:
+		print("""\nCategory [Bondage] Task: Hog Tie Your Sub For [As Long As You Want] This Is A Great Time To Interrogate Him,
+Tease Him, Put Your Feet In His Face, Bury His Face In Your Ass Or Pussy. You Can Keep Him Tied Up Based On Time,
+Or Until He Answers Enough Questions, Or Pleases You Enough. He's At Your Mercy.\n""")
+	if var == 47:
+		print("""\nCategory [Bondage]: Put Your Sub On A Leash & Collar To Show Them They're Your Bitch For [However Long You Want]
+Make Sure To Give The Leash A Tug Often, To Remind Them Who's In Charge.\n""")
+	if var == 48:
+		print("\nCategory [Feet]: Have Your Sub Kiss Your Feet For [1 Minute]\n")
+	if var == 49:
+		print("\nCategory [Feet]: Have Your Sub Kiss Your Feet [Until You're Satisfied]\n")
+	if var == 50:
+		print("\nCategory [Feet]: Have Your Sub Massage Your Feet [Until You're Satisfied]\n")
+	if var == 51:
+		print("\nCategory [Feet]: Tell Your Sub To Come Paint Your Nails.\n")
+	if var == 52:
+		print("\nCategory [Feet]: Have Your Sub Worship Your Feet [Until You're Satisfied]\n")
+	if var == 53:
+		print("\nCategory [Feet]: Tie Your Smelly Socks or Shoe Around Your Sub's Nose For [However Long You Want Them To Suffer]\n")
+	if var == 54:
+		print("""\nCategory [Feet]: Purposely/Secretly Tease Your Sub With Your Feet To Try To Get Them Hard And See How Long
+They Can Resist.\n""")
+	if var == 55:
+		print("\nCategory [Service]: Have Your Sub Do Anything You Want [1 Single Task]\n")
+	if var == 56:
+		print("\nCategory [Service]: Tell Your Sub To Go Clean Your [Bedroom] While You Go Relax\n")
+	if var == 57:
+		print("\nCategory [Service]: Tell Your Sub To Wash Your [Car] While You Go Relax\n")	
+	if var == 58:
+		print("\nCategory [Service]: Make Your Sub Bathe You (Shampoo Hair, Soap, etc)\n")
+	if var == 59:
+		print("\nCategory [Service] Task: Tell Your Sub To Go Do Your Laundry While You Relax.\n")
+	if var == 60:
+		print("\nCategory [Service]: Tell Your Sub To Go Run All Your Errands While You Go Relax\n")
+	if var == 61:
+		print("""\nCategory [Service]: Tell Your Sub To Go Make You A Sandwich (Or Whatever Food You Want) While You Relax,
+And Wait For Him To Bring It To You.\n""")
+	if var == 62:
+		print("\nCategory [Service]: Tell Your Sub To Go Do Your [Homework] While You Go Relax\n")
+	if var == 63:
+		print("\nCategory [Service]: Tell Your Sub To Go Do Your [Chores] While You Go Relax\n")	
+while True:
+	enter_number()
+	play_again = input("Please Press Enter If You Would Like To Generate Another Idea Ma'am.\n")
+	if play_again == "no":
+		break
